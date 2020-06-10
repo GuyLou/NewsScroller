6db20768108a82c91b4eb5c0c958db8bbada50ab
Guy L <guylou@outlook.com>
2020-06-10 14:01:28 +0300

    icon migration + old junk removal

--- app/items.js
+++ app/items.js
@@ -14,6 +14,9 @@
         if (typeof item.description == 'undefined') {
             item.description = '';
         }
+        if (137 < item.description.length) {
+            item.description = item.description.substring(0,Math.min(item.description.length,137)) + '...';
+        }
         dirclass = 'directionl';
         rtlDirCheck = new RegExp('^[^'+'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'+']*['+'\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC'+']');
         if (rtlDirCheck.test(item.title)) {
--- pub/js/popup-share.js
+++ pub/js/popup-share.js
@@ -19,7 +19,8 @@ class popupShare {
               	"email": {
             		"bootstrap":"warning",
             		"textcolor": "#ffe330",
-            		"fa":"envelope",
+					"fa":"envelope",
+					"icon":"envelope",
 					"linkbase":"mailto:?Body=",
 					"classes":""
             	},
@@ -27,7 +28,8 @@ class popupShare {
             		"bootstrap":"success",
             		"textcolor": "#ffffff",
             		"background":"#25D366",
-            		"fa":"whatsapp",
+					"fa":"whatsapp",
+					"icon":"whatsapp",
             		"linkbase":"whatsapp://send?text=",
 					"classes":""		
             	},
@@ -35,7 +37,8 @@ class popupShare {
             		"bootstrap":"info",
             		"textcolor": "#ffffff",
             		"background":"#1da1f2",
-            		"fa":"twitter-square",
+					"fa":"twitter-square",
+					"icon":"twitter-square",
             		"linkbase":"https://twitter.com/share?url=",
 					"classes":""
             	},
@@ -43,14 +46,14 @@ class popupShare {
             		"bootstrap":"primary",
             		"textcolor": "#ffffff",
             		"background":"#3b5998",
-            		"fa":"facebook-square",
+					"icon":"facebook",
             		"linkbase":"https://www.facebook.com/sharer/sharer.php?u=",
 					"classes":"" 		
             	},
             	"linkedin": {
             		"textcolor": "#ffffff",
             		"background":"#0e76a8",
-            		"fa":"linkedin-square", 
+					"icon":"linkedin",
             	    "linkbase":"https://www.linkedin.com/shareArticle?mini=true&url=",
 					"classes":""
             	}
@@ -102,7 +105,7 @@ class popupShare {
 		    if (this.destinations[key].hasOwnProperty("textcolor")) {
 		        linkstyle += 'color: ' + this.destinations[key]['textcolor'] + '; ';
 		    }
-			bar += '<li style="display: inline-block; zoom: 1; *display: inline; vertical-align: middle;"><a href="' + this.destinations[key].linkbase + this.sharelink + '" style="' + linkstyle + '" class="btn ' + this.destinations[key].classes + '"><i class="fa fa-' + this.destinations[key].fa + '"></i></a></li>'; //  text-' + this.destinations[key].bootstrap + '"
+			bar += '<li style="display: inline-block; zoom: 1; *display: inline; vertical-align: middle;"><a href="' + this.destinations[key].linkbase + this.sharelink + '" style="' + linkstyle + '" class="btn ' + this.destinations[key].classes + '"><span class="icon-' + this.destinations[key].icon + '"></span></a></li>'; //  text-' + this.destinations[key].bootstrap + '"
 
 		}
 		bar += '</ul></div>' + '\n';
